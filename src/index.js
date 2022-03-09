7ecd0a9dd6222ee241ddf7c93e8b31dd55bb4615
vikipretium <vighneshpasupathy@gmail.com>
2022-03-09 19:46:48 +0530

    add event to submit input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./style.css";
+import addPoints from "./assets/modules/add.js";
+
+//array for local storage
+let userInput = [];
+
+const addButton = document.querySelector(".bt-submit");
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  addPoints(event);
+});
