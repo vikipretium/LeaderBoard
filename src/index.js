eedd082960161ad8f2e0a2e6722b5360d2c57e9d
vikipretium <vighneshpasupathy@gmail.com>
2022-03-12 15:31:42 +0530

    fix linter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import "./style.css";
-import { getData } from "./assets/modules/api.js";
-import addPoints from "./assets/modules/add.js";
-import icon from "./assets/images/microverse_logo1.png";
+import './style.css';
+import { getData } from './assets/modules/api.js';
+import addPoints from './assets/modules/add.js';
+import icon from './assets/images/microverse_logo1.png';
 
-const formInput = document.querySelector(".form");
+const formInput = document.querySelector('.form');
 
-formInput.addEventListener("submit", (event) => {
+formInput.addEventListener('submit', (event) => {
   event.preventDefault();
   addPoints();
 });
-const refresh = document.querySelector(".refresh");
-refresh.addEventListener("click", (e) => {
+const refresh = document.querySelector('.refresh');
+refresh.addEventListener('click', (e) => {
   e.preventDefault();
   getData();
 });
@@ -19,7 +19,7 @@ window.onload = () => {
   getData();
 };
 
-const logo = document.createElement("img");
-logo.className = "icon";
+const logo = document.createElement('img');
+logo.className = 'icon';
 logo.src = icon;
-document.querySelector(".head").prepend(logo);
+document.querySelector('.head').prepend(logo);
